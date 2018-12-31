f121084e53280616013610e95616f9fe6e40e933
Eric <eric.schuler2@gmail.com>
2018-12-30 20:41:23 -0500

    Added initial stubbed out game class files (With main function)

---
+++ games/splendor/src/Game.cpp
@@ -0,0 +1,23 @@
+// Game.cpp
+//
+// Author: Eric Schuler
+// 
+// Main functionality for a Splendor game
+
+#include "Game.h"
+
+#include <iostream>
+
+namespace splendor
+{
+
+} // namespace splendor
+
+/*
+ * Main function
+ */
+int main()
+{
+  std::cout << "Hello world!\n";
+}
+  
---
+++ games/splendor/src/Game.h
@@ -0,0 +1,22 @@
+// Game.h
+//
+// Author: Eric Schuler
+// 
+// Main functionality for a Splendor game
+
+#ifndef SPLENDOR_GAME_H
+#define SPLENDOR_GAME_H
+
+#include <iostream>
+
+namespace splendor
+{
+
+  class Game
+  {
+
+  };
+  
+} // namespace splendor
+
+#endif // SPLENDOR_GAME_H
