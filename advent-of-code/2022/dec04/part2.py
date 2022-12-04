f7c5892cbceff071c66f7c1ef55bff6360699d2c
U-JHUAPL\schulem1 <schulem1@SCHULEM1-WL3.dom1.jhuapl.edu>
2022-12-04 09:11:12 -0500

    Advent of code 12/04/2022

---
+++ advent-of-code/2022/dec04/part2.py
@@ -0,0 +1,42 @@
+
+infile = "input.txt"
+
+sum = 0
+
+def contains(start, end, val):
+    return val >= start and val <= end
+
+def fully_contains(start1, end1, start2, end2):
+    return contains(start1, end1, start2) and contains(start1, end1, end2)
+    
+def overlaps(start1, end1, start2, end2):
+    # 43, 44, 7, 45
+    return contains(start1, end1, start2) or contains(start1, end1, end2)
+    
+count = 0
+
+for line in open(infile):
+    line = line.strip()
+    
+    line_split = line.split(",")
+    range1 = line_split[0]
+    range2 = line_split[1]
+    
+    range1_split = range1.split("-")
+    range1_s = int(range1_split[0])
+    range1_e = int(range1_split[1])
+    
+    range2_split = range2.split("-")
+    range2_s = int(range2_split[0])
+    range2_e = int(range2_split[1])
+    
+    overlapping = False
+    
+    if fully_contains(range1_s, range1_e, range2_s, range2_e) or fully_contains(range2_s, range2_e, range1_s, range1_e) or overlaps(range1_s, range1_e, range2_s, range2_e):
+        overlapping = True
+        count = count + 1
+        
+    print("{}: {}".format(line, overlapping))
+        
+print(count)
+
