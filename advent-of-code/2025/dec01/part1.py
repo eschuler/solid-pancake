bc6d2f825ea7a74bde880207e6d6a06acf4b0fda
Eric <eric.schuler@gmail.com>
2025-12-04 20:33:12 -0500

    (U) First cut at 20251201

---
+++ advent-of-code/2025/dec01/part1.py
@@ -0,0 +1,23 @@
+
+infile_path = "sample.txt"
+
+zero_count = 0
+curr_dial = 50
+
+regex = re.compile("(L|R)(\d+)")
+
+with open(infile_path, "r") as infile:
+
+    for line in infile:
+        line = line.strip()
+        match_obj = regex.match(line)
+        
+        if match_obj is None:
+            print("ERROR: " + line)
+            exit(1)
+            
+        groups = match_obj.groups()
+        direction = groups[0]
+        amount = int(groups[1])
+        
+        print("{} -> {}".format(direction, amount))
